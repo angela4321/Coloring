edaf9be7c95b8d42568b2152725e44603e8b5059
angela4321 <angelaqiao621@gmail.com>
2020-08-06 16:05:08 -0700

    Convert image to black and white and reverse color

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let pic = null;
 let loadButton;
 let inp;
+let bw = false;
 function setup(){
 
     createCanvas(500,500);
@@ -21,8 +22,8 @@ function load(){
 
 
 function blackAndWhite(){
-
-    for(let i = 0; i < pic.width*pic.height; i++){
+    pic.loadPixels();
+    for(let i = 0; i < 4*pic.width*pic.height; i+=4){
         let sum = 0;
 
         for(let j = 0; j < 3; j++){
@@ -34,23 +35,49 @@ function blackAndWhite(){
             val = 0;
         }
         else{
-            val=100;
+            val=255;
         }
         for(let j = 0; j < 3; j++){
             pic.pixels[i+j]=val;
             
         }
-        i+=3;
     }
+    pic.updatePixels();
 
 }
 //makes the black pixels white and the white pixels black, only valid before coloring starts
 function rev(){
+    pic.loadPixels();
+    for(let i = 0; i < 4*pic.width*pic.height; i+=4){
+        let sum = 0;
 
+        for(let j = 0; j < 3; j++){
+            sum+=pic.pixels[i+j];
+            
+        }
+        let val;
+        if(sum/3<255/2){
+            val = 255;
+        }
+        else{
+            val=0;
+        }
+        for(let j = 0; j < 3; j++){
+            pic.pixels[i+j]=val;
+            
+        }
+    }
+    pic.updatePixels();
 }
 
 function draw(){
+    
     if(pic){
+        if(!bw && pic.width>1){
+            blackAndWhite();
+            rev();
+            bw = true;
+        }
         image(pic,0,0,width,height);
     }
     else{
